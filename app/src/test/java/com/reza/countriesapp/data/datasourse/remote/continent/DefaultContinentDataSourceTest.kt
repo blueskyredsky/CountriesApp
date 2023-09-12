f9a26da01758c011fd3880be4c16a6860386a874
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-09-12 20:28:13 +0100

    workig on mocking apollo response

--- app/src/test/java/com/reza/countriesapp/data/datasourse/remote/continent/DefaultContinentDataSourceTest.kt
+++ app/src/test/java/com/reza/countriesapp/data/datasourse/remote/continent/DefaultContinentDataSourceTest.kt
@@ -1,10 +1,15 @@
 package com.reza.countriesapp.data.datasourse.remote.continent
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.annotations.ApolloExperimental
 import com.apollographql.apollo3.mockserver.MockRequest
 import com.apollographql.apollo3.mockserver.MockResponse
 import com.apollographql.apollo3.mockserver.MockServer
 import com.apollographql.apollo3.mockserver.MockServerHandler
+import com.apollographql.apollo3.testing.QueueTestNetworkTransport
+import com.apollographql.apollo3.testing.enqueueTestResponse
+import com.reza.ContinentsQuery
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -15,22 +20,29 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @OptIn(ExperimentalCoroutinesApi::class)
-//@RunWith(MockitoJUnitRunner::class)
 class DefaultContinentDataSourceTest {
-
-    @Mock
-    private lateinit var apolloClient: ApolloClient
 
     private lateinit var continentDataSource: ContinentDataSource
 
+    @OptIn(ApolloExperimental::class)
     @Before
     fun setUp() {
 
     }
 
+    @OptIn(ApolloExperimental::class)
     @Test
     fun `should getContinents be called`() = runTest {
+        val apolloClient = ApolloClient.Builder()
+            .networkTransport(QueueTestNetworkTransport())
+            .build()
 
+        val testQuery = ContinentsQuery()
+        val testData = ContinentsQuery.Data(listOf(ContinentsQuery.Continent(name = "Africa", code = "AF")))
+        apolloClient.enqueueTestResponse(testQuery, testData)
+
+        val actual = apolloClient.query(testQuery).execute().data!!
+        assertEquals(testData.continents.first().name, actual.continents.first().name)
     }
 
 
