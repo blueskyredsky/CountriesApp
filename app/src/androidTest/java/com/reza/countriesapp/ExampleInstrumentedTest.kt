cfb096a12d720a596ab80656d1dfb1a60a2102be
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-10-22 12:23:41 +0100

    adding first ui test

--- app/src/androidTest/java/com/reza/countriesapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/reza/countriesapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.reza.countriesapp
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.reza.countriesapp.presentation.MainActivity
+import com.reza.countriesapp.presentation.common.LoadingItem
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +22,20 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun test() {
+        composeTestRule.setContent {
+            LoadingItem()
+        }
+
+        composeTestRule
+            .onNodeWithTag(testTag = "myProgressIndicator")
+            .assertIsDisplayed()
+    }
     @Test
     fun useAppContext() {
         // Context of the app under test.
--- app/src/main/java/com/reza/countriesapp/presentation/common/Common.kt
+++ app/src/main/java/com/reza/countriesapp/presentation/common/Common.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import com.reza.countriesapp.ui.theme.CountriesAppTheme
 
@@ -15,7 +16,7 @@ fun LoadingItem(modifier: Modifier = Modifier) {
         modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        CircularProgressIndicator(Modifier.testTag("myProgressIndicator"))
     }
 }
 
