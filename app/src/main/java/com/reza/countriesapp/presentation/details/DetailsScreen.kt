27947fa00020ada5bb1d8a045b22a7f28b7dfe92
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2024-05-02 08:10:06 +0100

    working on making customUiStateHolder for search functionality

--- app/src/main/java/com/reza/countriesapp/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/reza/countriesapp/presentation/details/DetailsScreen.kt
@@ -3,21 +3,19 @@ package com.reza.countriesapp.presentation.details
 import android.content.res.Configuration
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -28,6 +26,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
@@ -43,18 +42,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.EmojiSupportMatch
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.reza.countriesapp.R
-import com.reza.countriesapp.domain.model.Continent
 import com.reza.countriesapp.domain.model.Country
 import com.reza.countriesapp.presentation.common.LoadingItem
-import com.reza.countriesapp.presentation.home.ContinentItem
-import com.reza.countriesapp.presentation.home.ContinentView
 import com.reza.countriesapp.presentation.home.HomeEvent
 import com.reza.countriesapp.ui.theme.CountriesAppTheme
 import com.reza.countriesapp.util.Constants
@@ -81,6 +74,11 @@ fun DetailsScreen(
                 navigationIcon = {
                     IconButton(onClick = onBackClick) {
                         Icon(Icons.Default.KeyboardArrowLeft, contentDescription = "back")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {  }) {
+                        Icon(Icons.Default.Search, contentDescription = "back")
                     }
                 }
             )
