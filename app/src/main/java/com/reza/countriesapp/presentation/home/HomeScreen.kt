b7384407bfb5620449a133578328094958e4d789
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2024-05-03 08:13:53 +0100

    working on ui screen state holder

--- app/src/main/java/com/reza/countriesapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/reza/countriesapp/presentation/home/HomeScreen.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -58,6 +56,8 @@ fun ContinentsScreen(
 ) {
     val state by viewModel.continentsState.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
+    // todo working on removing ui related logic states
+    val screenState = rememberHomeScreenState()
     Scaffold(
         topBar = {
             TopAppBar(
--- app/src/main/java/com/reza/countriesapp/presentation/home/HomeState.kt
+++ app/src/main/java/com/reza/countriesapp/presentation/home/HomeState.kt
@@ -1,7 +1,16 @@
 package com.reza.countriesapp.presentation.home
 
 import androidx.annotation.DrawableRes
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.reza.countriesapp.domain.model.Continent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import javax.annotation.concurrent.Immutable
 
 @Immutable
@@ -15,3 +24,29 @@ data class ContinentView(
     val continent: Continent,
     @DrawableRes val imageResource: Int
 )
+
+@Stable
+class HomeStateHolder(
+    private val snackBarHostState: SnackbarHostState,
+    private val scope: CoroutineScope
+) {
+    fun showSnackBar(
+        message: String,
+        actionLabel: String,
+        resultCallback: (SnackbarResult) -> Unit
+    ) {
+        scope.launch {
+            val result =
+                snackBarHostState.showSnackbar(message = message, actionLabel = actionLabel)
+            resultCallback(result)
+        }
+    }
+}
+
+@Composable
+fun rememberHomeScreenState(
+    snackBarHostState: SnackbarHostState = remember { SnackbarHostState() },
+    scope: CoroutineScope = rememberCoroutineScope(),
+) = remember {
+    HomeStateHolder(scope = scope, snackBarHostState = snackBarHostState)
+}
