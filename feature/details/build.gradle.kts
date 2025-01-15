b3002f783ecd47e756ceb220c56e76db135d08e6
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2025-01-15 07:55:56 +0000

    datasource of details module is fixed

--- feature/details/build.gradle.kts
+++ feature/details/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    kotlin("kapt") // todo change that to alias
+    id("kotlin-parcelize") // todo change that to alias
 }
 
 android {
@@ -35,10 +38,43 @@ android {
 
 dependencies {
 
+    // projects
+    implementation(project(":core:threading"))
+    implementation(project(":core:networking"))
+    implementation(project(":core:common"))
+    implementation(project(":core:designsystem"))
+    testImplementation(project(":core:testing:unit"))
+
+
+    implementation(libs.javax.inject)
+
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.google.material)
-    testImplementation(libs.junit)
+
+    // Compose
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation(libs.activity.compose)
+    implementation(libs.compose.lifecycle)
+    implementation(libs.material3)
+    implementation(libs.material)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.ui.tooling.preview)
+
+    // hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.kapt)
+    implementation(libs.hilt.navigation.compose)
+
+    // ui test
+    androidTestImplementation(libs.ui.test.junit4)
+    androidTestImplementation(libs.truth)
+    androidTestImplementation(libs.hilt.test)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.navigation.test)
+    androidTestImplementation(libs.androidx.runner)
+    kaptAndroidTest(libs.hilt.kapt.test)
 }
---
+++ feature/details/src/test/java/com/reza/details/data/datasource/remote/countries/DefaultCountriesDateSourceTest.kt
@@ -0,0 +1,63 @@
+package com.reza.details.data.datasource.remote.countries
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.annotations.ApolloExperimental
+import com.apollographql.apollo.testing.QueueTestNetworkTransport
+import com.apollographql.apollo.testing.enqueueTestResponse
+import com.google.common.truth.Truth.assertThat
+import com.reza.ContinentQuery
+import com.reza.type.buildContinent
+import com.reza.type.buildCountry
+import com.reza.type.buildLanguage
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class DefaultCountriesDateSourceTest {
+
+    private lateinit var apolloClient: ApolloClient
+
+    @OptIn(ApolloExperimental::class)
+    @Before
+    fun setUp() {
+        apolloClient = ApolloClient.Builder()
+            .networkTransport(QueueTestNetworkTransport())
+            .build()
+    }
+
+    @OptIn(ApolloExperimental::class)
+    @Test
+    fun `testing getContinent`() = runTest {
+        // Given
+        val query = ContinentQuery("AF")
+        val data = ContinentQuery.Data {
+            continent = buildContinent {
+                countries = listOf(
+                    buildCountry {
+                        name = "Angola"
+                        emoji = "\uD83C\uDDE6"
+                        currency = "AOA"
+                        capital = "Luanda"
+                        phone = "244"
+                        languages = listOf(buildLanguage {
+                            name = "Portuguese"
+                        })
+                        states = listOf()
+                    })
+            }
+        }
+        apolloClient.enqueueTestResponse(query, data)
+
+        // When
+        val actual = apolloClient
+            .query(query)
+            .execute()
+            .data
+
+        // Then
+        assertThat(data.continent?.countries?.first()?.currency)
+            .isEqualTo(actual?.continent?.countries?.first()?.currency)
+        assertThat(data.continent?.countries?.first()?.name)
+            .isEqualTo(actual?.continent?.countries?.first()?.name)
+    }
+}
